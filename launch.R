fbec5fee2ff90d56a2c82b085c997d343110f5ca
nderian <nicolas.derian@outlook.com>
2018-10-10 14:28:34 +0200

    New entries

--- launch.R
+++ launch.R
@@ -1,1 +1,25 @@
 # Launch code
+
+# Load libraries
+library("tidyverse")
+library("lubridate")
+# Import data
+play <- read_csv("P:/Work/Projects/z_Perso/F1/F1_1213.csv")
+# Set teams
+L <- as.matrix(unlist(unique(play[, "HomeTeam"])))
+# Number games
+gamePlayed <- apply(L, 1, gameP, play)
+# Goals for and against
+GoalsFor <- apply(L, 1, getGoalsF, play)
+GoalsAgainst <- apply(L, 1, getGoalsA, play)
+# Average goals for and against
+avgGoalForHome <- GoalsFor[1,]/ gamePlayed[1,]
+avgGoalAgainstHome <- GoalsAgainst[1,] / gamePlayed[1,]
+avgGoalForAway <- GoalsFor[2,] / gamePlayed[1,]
+avgGoalAgainstAway <- GoalsAgainst[2,] / gamePlayed[2,]
+# Attack and defense strength
+attStrenHome <- avgGoalForHome / mean(avgGoalForHome)
+defStrenHome <- avgGoalAgainstHome / mean(avgGoalAgainstHome)
+attStrenAway <- avgGoalForAway / mean(avgGoalForAway)
+defStrenAway <- avgGoalAgainstAway / mean(avgGoalAgainstAway)
+
--- toLoad.r
+++ toLoad.r
@@ -1,3 +1,29 @@
 # Collection of functions for my project
 
 # Get goals for home and away teams
+# x is a dataframe and L a team
+getGoalsF <- function(L, x) {
+    locHome <- grep(L, unlist(x[, "HomeTeam"]))
+    locAway <- grep(L, unlist(x[, "AwayTeam"]))
+    goalsHome <- sum(x[locHome, "FTHG"])
+    goalsAway <- sum(x[locAway, "FTAG"])
+    res <- c(goalsHome, goalsAway)
+    return(res)
+}
+
+getGoalsA <- function(L, x) {
+    locHome <- grep(L, unlist(x[, "HomeTeam"]))
+    locAway <- grep(L, unlist(x[, "AwayTeam"]))
+    goalsHome <- sum(x[locHome, "FTAG"])
+    goalsAway <- sum(x[locAway, "FTHG"])
+    res <- c(goalsHome, goalsAway)
+    return(res)
+}
+
+# Number of games played
+gameP <- function(L, x) {
+    gpH <- sum(grepl(L, unlist(x[, "HomeTeam"])))
+    gpA <- sum(grepl(L, unlist(x[, "AwayTeam"])))
+    gp <- c(gpH, gpA)
+    return(gp)
+}
